9461dfec92f84f7e5b4040edda0d43d3db30e2f6
aman-mundra <aman.s.mundra@gmail.com>
2015-05-10 17:08:24 -0400

    Migrations Completed, Project Running.

--- Project/Project/CheckBookVM.cs
+++ Project/Project/CheckBookVM.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Model;
 using System.Collections.ObjectModel;
-namespace Checkbook.Mundra
+namespace Checkbook
 {
     public class CheckBookVM : BaseVM
     {
--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -10,25 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Checkbook
+namespace WpfApplication1
 {
     /// <summary>
-    /// Interaction logic for CheckBookWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var VM = new CheckBookVM();
-            DataContext = VM;
-            VM.Fill();
         }
     }
 }
