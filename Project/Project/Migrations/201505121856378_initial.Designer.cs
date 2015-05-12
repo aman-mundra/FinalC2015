83d07eb3d02a94833e8381e1372ed5719eafc453
aman-mundra (Aman) <aman.s.mundra@gmail.com>
2015-05-12 15:12:00 -0400

    Working with Database Present (Outdated Cafe)

--- Project/Project/Migrations/201505121856378_initial.Designer.cs
+++ Project/Project/Migrations/201505121856378_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Project.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201505102105148_InitialCreate"; }
+            get { return "201505121856378_initial"; }
         }
         
         string IMigrationMetadata.Source
